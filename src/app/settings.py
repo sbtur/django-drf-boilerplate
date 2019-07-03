c5a18c8f2a0a2154dbdfc7850208f801fabf5fdf
Mauricio de Mello <mauriciors@gmail.com>
2019-07-03 19:18:57 -0300

    feat: configure postgree database

--- src/app/settings.py
+++ src/app/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {"default": env.db()}
+DATABASES = {"default": env.db()}
 
 
 # Password validation

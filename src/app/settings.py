a5793aa59e10928018a92f1d361e8d5dc0dec5bb
Mauricio de Mello <mauriciors@gmail.com>
2019-07-11 15:16:01 -0300

    Fix: debug_tollbar in docker environ

--- src/app/settings.py
+++ src/app/settings.py
@@ -61,7 +61,9 @@ MIDDLEWARE = [
 if DEBUG:
     INSTALLED_APPS += ["debug_toolbar"]
     MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-    INTERNAL_IPS = "127.0.0.1"
+    DEBUG_TOOLBAR_CONFIG = {
+        "SHOW_TOOLBAR_CALLBACK" : lambda request: True,
+    }
 
 ROOT_URLCONF = "app.urls"
 
